28ac18aecf52301602fd9c2846f525df8197003d
chobobdev <chobobdev@naver.com>
2022-01-20 02:20:42 +0900

    FEAT: Redirect pages

--- main.py
+++ main.py
@@ -1,11 +1,41 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,redirect
 from functions import scrape as sc
 app = Flask(__name__)
+@app.route('/result/language=<language>&wwr&so')
+def so_wwr_redirect(language):
+    return redirect(f"/result/language={language}&so&wwr")
+
+@app.route('/result/language=<language>&ro&so')
+def so_ro_redirect(language):
+    return redirect(f"/result/language={language}&so&ro")
+
+@app.route('/result/language=<language>&ro&wwr')
+def wwr_ro_redirect(language):
+    return redirect(f"/result/language={language}&wwr&ro")
+
+@app.route('/result/language=<language>&wwr&ro')
+def wwr_ro_site(language):
+    return language+"wwrro"
+
+@app.route('/result/language=<language>&so&ro')
+def so_ro_site(language):
+    return language+"soro"
+
+@app.route('/result/language=<language>&so&wwr')
+def so_wwr_site(language):
+    return language+"sowwr"
+
+@app.route('/result/language=<language>&wwr')
+def wwr_site(language):
+    return language+"wwr"
+
+@app.route('/result/language=<language>&ro')
+def ro_site(language):
+    return language+"ro"
 
 @app.route('/result/language=<language>&so')
 def so_site(language):
     return language+"so"
-
 
 @app.route('/result/language=<language>')
 def all_site(language):
