b46eee2e3864b0830a4d2ff4244f09995ab7a9ca
chobobdev <chobobdev@naver.com>
2022-02-01 04:19:28 +0900

    FEAT: [WIP] jobs not found

--- main.py
+++ main.py
@@ -17,6 +17,11 @@ def so_ro_redirect(language):
 def wwr_ro_redirect(language):
     return redirect(f"/result/language={language}/wwr&ro")
 
+@app.route('/notfound=<language>')
+def job_not_found(language):
+    return render_template('notfound.html',language=language)
+
+
 @app.route('/result/language=<language>/wwr&ro',methods=['GET','POST'])
 def wwr_ro_site(language):
     prev_time=ut.check_time(language)
@@ -34,7 +39,7 @@ def wwr_ro_site(language):
                     return(redirect(f'/result/language={language}/wwr&ro'))
             return render_template('result.html',jobs=jobs,langlogo=f"/static/img/{language.lower()}-logo.png",language=language,lastupdate=0)
         else:
-            return(redirect("/"))
+            return(redirect(f"/notfound={language}"))
     else:
         json_job=ut.load_json(language)
         jobs=ut.wwr_dict(json_job)+ut.ro_dict(json_job)
@@ -65,7 +70,7 @@ def so_ro_site(language):
                     return(redirect(f'/result/language={language}/so&ro'))
             return render_template('result.html',jobs=jobs,langlogo=f"/static/img/{language.lower()}-logo.png",language=language,lastupdate=0)
         else:
-            return(redirect("/"))
+            return(redirect(f"/notfound={language}"))
     else:
         json_job=ut.load_json(language)
         jobs=ut.so_dict(json_job)+ut.ro_dict(json_job)
@@ -96,7 +101,7 @@ def so_wwr_site(language):
                     return(redirect(f'/result/language={language}/so&wwr'))
             return render_template('result.html',jobs=jobs,langlogo=f"/static/img/{language.lower()}-logo.png",language=language,lastupdate=0)
         else:
-            return(redirect("/"))
+            return(redirect(f"/notfound={language}"))
     else:
         json_job=ut.load_json(language)
         jobs=ut.so_dict(json_job)+ut.wwr_dict(json_job)
@@ -127,7 +132,7 @@ def wwr_site(language):
                     return(redirect(f'/result/language={language}/wwr'))
             return render_template('result.html',jobs=jobs,langlogo=f"/static/img/{language.lower()}-logo.png",language=language,lastupdate=0)
         else:
-            return(redirect("/"))
+            return(redirect(f"/notfound={language}"))
     else:
         jobs=ut.wwr_dict(ut.load_json(language))
         if request.method == 'POST':
@@ -156,7 +161,7 @@ def ro_site(language):
                     return(redirect(f'/result/language={language}/ro'))
             return render_template('result.html',jobs=jobs,langlogo=f"/static/img/{language.lower()}-logo.png",language=language,lastupdate=0)
         else:
-            return(redirect("/"))
+            return(redirect(f"/notfound={language}"))
     else:
         jobs=ut.ro_dict(ut.load_json(language))
         if request.method == 'POST':
@@ -186,7 +191,7 @@ def so_site(language):
                     return(redirect(f'/result/language={language}/so'))
             return render_template('result.html',jobs=jobs,langlogo=f"/static/img/{language.lower()}-logo.png",language=language,lastupdate=0)
         else:
-            return(redirect("/"))
+            return(redirect(f"/notfound={language}"))
     else:
         jobs=ut.so_dict(ut.load_json(language))
         if request.method == 'POST':
@@ -215,7 +220,7 @@ def all_site(language):
                     return(redirect(f'/result/language={language}/'))
             return render_template('result.html',jobs=jobs,langlogo=f"/static/img/{language.lower()}-logo.png",language=language,lastupdate=0)
         else:
-            return(redirect("/"))
+            return(redirect(f"/notfound={language}"))
     else:
         jobs=ut.load_json(language)
         if request.method == 'POST':
